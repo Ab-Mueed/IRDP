f2a8d663b17083c6cd24b3c63db1835e106a26c9
Ab-Mueed <studymueed@gmail.com>
2024-12-05 09:57:44 +0530

    Changes in NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ export default function NavBar() {
     <nav className="border-b">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
-          <a href="/" className="text-2xl font-semibold">Job Analysis</a>
+          <a href="/" className="text-xl font-semibold py-3 md:text-2xl">HireFit - <span className="italic text-sm md:text-lg"> Resume-Job Compatibility Check</span></a>
         </div>
       </div>
     </nav>
