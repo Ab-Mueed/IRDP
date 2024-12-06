e5137a697bd509cca55fec668b10fd2b3aecaa82
Ab-Mueed <studymueed@gmail.com>
2024-12-07 00:24:12 +0530

    Added fontSize in Hero.jsx textReveal Card

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -7,7 +7,7 @@ import {
 export default function Hero({ rating }) {
   return (
     <div className="flex items-center justify-center h-auto sm:h-[20rem] mt-4 rounded-2xl w-full px-4">
-      <TextRevealCard text="Reveal Rating" revealText={rating}>
+      <TextRevealCard className="text-lg md:text-sm" text="Reveal Rating" revealText={rating}>
         <TextRevealCardTitle className="text-2xl">
           Find out how well your resume matches a job description.
         </TextRevealCardTitle>
